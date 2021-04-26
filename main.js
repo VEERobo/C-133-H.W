bda5c4b7398f0c8407dcbe27ecc04032aea6e7b0
VEERobo <71770036+VEERobo@users.noreply.github.com>
2021-04-26 20:35:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bed(){
+    window.location = "1st image.html";
+}
+function tv(){
+    window.location = "2nd.html";
+}
+function desk(){
+    window.location = "3rd.html";
+}
+function bottle(){
+    window.location = "4th.html";
+}
+function fruit(){
+    window.location = "5th.html";
+}
